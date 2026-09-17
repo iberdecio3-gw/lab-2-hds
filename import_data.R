f907d5283732038104c7ee69aac95eec80706fa4
Isabel Berdecio <iberdecio3@gwu.edu>
2026-09-16 20:17:56 -0400

    Update gene expression analysis notebook and markdown

--- import_data.R
+++ import_data.R
@@ -1,6 +1,20 @@
 url <- paste0("https://www.ncbi.nlm.nih.gov/geo/download/",
               "?acc=GSE52778&format=file&file=GSE52778_All_Sample_FPKM_Matrix.txt.gz")
 
-download.file(url, "fpkm.txt.gz", mode = "wb")
+# Download the file
+download.file(
+  url,
+  "GSE52778_All_Sample_FPKM_Matrix.txt.gz",
+  mode = "wb"
+)
 
-fpkm <- read.delim(gzfile("fpkm.txt.gz"))
+# Read the compressed tab-delimited file
+fpkm <- read.table(
+  gzfile("GSE52778_All_Sample_FPKM_Matrix.txt.gz"),
+  header = TRUE,
+  sep = "",
+  check.names = FALSE
+)
+
+dim(fpkm)
+colnames(fpkm)
